3cc6f463600cc22dffcebd7f5eaf75ec3ceca81a
mouli-16 <mouli1216@gmail.com>
2022-08-26 02:03:49 +0530

    Changed Appearance of QR Code scanner

--- frontend/components/QrScanner.js
+++ frontend/components/QrScanner.js
@@ -1,64 +1,83 @@
-import React, { Component } from 'react'
-import QrReader from 'react-qr-scanner'
+import React, { Component } from "react";
+import QrReader from "react-qr-scanner";
+import verify from "../public/assets/verify.gif";
+import Image from "next/image";
 
 class QrContainer extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       delay: 100,
-      result: 'No result',
-    }
+      result: "No result",
+    };
 
-    this.handleScan = this.handleScan.bind(this)
+    this.handleScan = this.handleScan.bind(this);
   }
-  handleScan(result){
+  handleScan(result) {
     console.log(result);
-    
+
     this.setState({
-      result: (result != null)?result.text:"No result",
-    })
-  
+      result: result != null ? result.text : "No result",
+    });
   }
-  handleError(err){
-    console.error(err)
+  handleError(err) {
+    console.error(err);
   }
-  render(){
+  render() {
     const previewStyle = {
       height: 700,
-      width: '100%',
-      display:'flex',
-        "justify-content":"center"
-    }
+      width: "100%",
+      display: "flex",
+      "justify-content": "center",
+    };
 
-    const camStyle={
-      display:'flex',
-      justifyContext:"center",
-      marginTop:"-50px"
-    }
+    const camStyle = {
+      display: "flex",
+      justifyContext: "center",
+      marginTop: "-50px",
+    };
 
     const textStyle = {
-      fontSize:"30px",
-      "text-align":"center",
-      marginTop:'-50px'
-    }
+      fontSize: "30px",
+      "text-align": "center",
+      marginTop: "-50px",
+    };
 
-    return(
+    return (
       <React.Fragment>
-        
-        { this.state.result=="No result" &&
+        {this.state.result != "123" ? (
           <div style={camStyle}>
-          <QrReader
-          ref = {this.props.qr}
-          delay={100}
-          style={previewStyle}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          />
-        </div>}
-        <p style={textStyle}>{this.state.result == '123' ? "Verified" : "Not Verified"} </p>  
+            <QrReader
+              ref={this.props.qr}
+              delay={100}
+              style={previewStyle}
+              onError={this.handleError}
+              onScan={this.handleScan}
+            />
+          </div>
+        ) : (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Image
+              src={verify}
+              alt="Picture of the author"
+              width="600px"
+              height="640px"
+
+              // layout="responsive"
+            />
+          </div>
+        )}
+        {/* <p style={textStyle}>
+          {this.state.result == "123" ? "Verified" : "Not Verified"}{" "}
+        </p> */}
       </React.Fragment>
-    )
+    );
   }
 }
 
-export default  QrContainer;
+export default QrContainer;
---
+++ frontend/pages/scanner.js
@@ -0,0 +1,53 @@
+import Head from "next/head";
+import Scanner from "../components/QrScanner";
+import ContactComponent from "../components/ContactComponent";
+import { useState,useRef } from "react";
+import QrReader from 'react-qr-scanner'
+
+
+const qr = useRef<QrReader>(null)
+
+
+export default function Home() {
+  const [isAdmin, setIsAdmin] = useState(true);
+  return (
+    <div>
+      <Head>
+        <title>SEAGI-LOCKER - Landing Page</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+          crossorigin="anonymous"
+        ></link>
+      </Head>
+
+      <Scanner qr={qr}/>
+      <ContactComponent />
+      <footer className="footer">
+        <h5> &copy; Ministry of Ports, Shipping & Waterways</h5>
+        <script
+          src="https://unpkg.com/react/umd/react.production.min.js"
+          crossorigin
+        ></script>
+
+        <script
+          src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+          crossorigin
+        ></script>
+
+        <script
+          src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+          crossorigin
+        ></script>
+      </footer>
+    </div>
+  );
+}
